73808e5a0d0eb8cfa5f9c8cd8aff8146d5b9ef30
niceben <niceben@qq.com>
2019-06-28 15:33:10 +0800

    add web configration

--- src/main/java/com/zsl/mavengit/MavengitApplication.java
+++ src/main/java/com/zsl/mavengit/MavengitApplication.java
@@ -2,12 +2,19 @@ package com.zsl.mavengit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MavengitApplication {
+public class MavengitApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MavengitApplication.class, args);
     }
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MavengitApplication.class);
+    }
 }
